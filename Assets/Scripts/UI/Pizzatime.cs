1f17e3af5b53b6b42e067134b9bfd54c2a076ff8
Kelsey Davis <s179124@students.aie.edu.au>
2018-09-25 16:20:09 -0700

    Second Commit

--- Assets/Scripts/UI/Pizzatime.cs
+++ Assets/Scripts/UI/Pizzatime.cs
@@ -3,26 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpawnOnClick : MonoBehaviour {
+public class Pizzatime : MonoBehaviour
+{
 
     public GameObject Prefab;
+    public AudioClip pizzatime;
+    private AudioSource source;
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
     public Button SpawnButton;
 
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         Button btn = SpawnButton.GetComponent<Button>();
         btn.onClick.AddListener(TaskOnClick);
+        source = GetComponent<AudioSource>();
     }
-	
-	// Update is called once per frame
-	void TaskOnClick()
+
+    // Update is called once per frame
+    void TaskOnClick()
     {
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-        Instantiate(Prefab, spawnPoints[spawnPointIndex].position, 
+        source.PlayOneShot(pizzatime);
+        Instantiate(Prefab, spawnPoints[spawnPointIndex].position,
             spawnPoints[spawnPointIndex].rotation);
     }
 }
